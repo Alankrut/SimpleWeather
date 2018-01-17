4ee8e9c47b5c2de00dde7b010eb2e97d5497f864
Alankrut <alankrut@gmail.com>
2018-01-16 21:43:59 -0600

    Created a 'Weather' data class in Kotlin, to be populated with weather data. Currently testing invoking kotlin object via java in the MainActivity, also had to update build.gradle to account for kotlin sources. Added location permission to AndroidManifest for the upcoming Awareness API, as well as the meta-data for the API Key.

--- app/src/main/java/com/systemxstudios/weather/MainActivity.java
+++ app/src/main/java/com/systemxstudios/weather/MainActivity.java
@@ -3,10 +3,17 @@ package com.systemxstudios.weather;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.systemxstudios.object.Weather;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_text) TextView tvMain;
 
     /**
      * Overriding the system baseContext in order to use custom fonts. Done so by wrapping the
@@ -21,5 +28,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        // Testing creating a kotlin object within java
+        Float fTemp = 25.0f;
+        Float fTempFeelsLike = 2.0f;
+        Float fDew = 2.0f;
+        int numHumidity = 2;
+        int[] arrConditions = new int[] {1,2,3};
+
+        Weather weather = new Weather(fTemp,fTempFeelsLike,numHumidity,fDew,arrConditions);
+        tvMain.setText(String.valueOf(weather.getTemp()));
     }
 }
